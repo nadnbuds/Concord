16a5c4014e63157084781b02d13b5f5766ee2c04
nadnbuds <lalam003@ucr.edu>
2018-10-24 16:57:04 -0700

    Base Scripts

--- Assets/Scripts/Items/WeaponStat.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public struct WeaponStat {
-    public float variation;
-    public float damage;
-    public float range;
-}
--- Assets/Scripts/Managers/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class PlayerData{
-    public string name;
-    public Role playerRole;
-}
